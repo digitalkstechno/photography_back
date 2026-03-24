a20767e37314f04a1a4637378592ef27aaf6d71b
qureshiowaisdigitalks <owais@digitalks.co.in>
2026-03-24 10:31:43 +0530

    fix

--- src/seed.js
+++ src/seed.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const mongoUri = "mongodb+srv://photo:23XamLihXQ18bfb4@digitalks.emxt3a4.mongodb.net/photography_studio";
+    const mongoUri = "";
     const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
